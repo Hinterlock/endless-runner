e7ffa0149645e0426625dad829266a22ee3d672b
Hinterlock <102774385+Hinterlock@users.noreply.github.com>
2022-04-20 20:02:22 -0700

    added menu functionality

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 //Endless Runner Project
+//Authors: Aidan Adams, Lee Coronilia, Kirsten Lindblad, Angela Yim
 let config = {
     type: Phaser.AUTO,
-    width: 640,
-    height: 480,
+    width: 970,
+    height: 600,
+    backgroundColor: '#1d3a69',
     scene: [ Menu, Play ]
 }
 let game = new Phaser.Game(config);
 // set UI sizes
-let borderUISize = game.config.height / 15;
+let borderUISize = game.config.height / 20;
 let borderPadding = borderUISize / 3;
 // reserve keyboard vars
+let keyA, keyD;
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,14 +9,31 @@ class Menu extends Phaser.Scene {
 
     create() {
         this.add.text(20, 20, "Menu");
-        this.scene.start("playScene");
 
         // text config
+        let menuConfig = {
+            fontFamily: 'Comic Sans',
+            fontSize: '28px',
+            backgroundColor: '#f5d41b',
+            color: '#1d3a69',
+            align: 'middle',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        // menu text
+        this.add.text(game.config.width/2, game.config.height/2 - 2*(borderUISize + borderPadding), ' Endless Runner ', menuConfig).setOrigin(0.5);
 
         // define keys
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
     update() {
-        
+        if (Phaser.Input.Keyboard.JustDown(keyD)) {
+            this.scene.start("playScene");
+        }
     }
 }
