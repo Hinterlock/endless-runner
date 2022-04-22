9a70c57f05f475198f6b4e73eee19f346e85993c
Lee Coronilia <shadowwolf246815@gmail.com>
2022-04-21 18:09:21 -0700

    added assets and clouds / notebook bg in scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,9 @@ class Play extends Phaser.Scene {
         this.load.image('guy_stand', './assets/guy_stand.png');
         this.load.image('guy_slide', './assets/guy_slide.png');
         this.load.image('bus', './assets/bus.png');
+        this.load.image('notebookbg', './assets/notebookbg.png');
+        this.load.image('clouds', './assets/clouds.png');
+        this.load.image('ground', './assets/ground.png');
         // load spritesheet
         //this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
@@ -16,8 +19,12 @@ class Play extends Phaser.Scene {
         //this.add.text(20, 20, "Play");
         //console.log('pog');
 
+        //background
+        this.notebookbg = this.add.tileSprite(0, 0, 970, 600, 'notebookbg').setOrigin(0, 0);
+        this.clouds = this.add.tileSprite(0, 0, 0, 0, 'clouds').setOrigin(0, 0);
+
         this.ground = this.physics.add.staticGroup();
-        this.ground.create(game.config.width/2, game.config.height + borderPadding*10, 'bus');
+        this.ground.create(game.config.width/2, game.config.height + borderPadding*10, 'bus'); //hm i can't get the ground png to show?
         
         this.p1Guy = this.physics.add.sprite(game.config.width/2, game.config.height/2, 'guy_stand').setScale(0.3);
 
